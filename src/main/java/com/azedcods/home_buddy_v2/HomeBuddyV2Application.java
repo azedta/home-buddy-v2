22b7f6c0c5d8f64ca786812d119586c664d61d1e
Aziz Tazrout <aztazrout@gmail.com>
2025-12-19 15:37:16 -0500

    Initialize Home Buddy v2 backend project

---
+++ src/main/java/com/azedcods/home_buddy_v2/HomeBuddyV2Application.java
@@ -0,0 +1,13 @@
+package com.azedcods.home_buddy_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBuddyV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBuddyV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/azedcods/home_buddy_v2/HomeBuddyV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azedcods.home_buddy_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBuddyV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
