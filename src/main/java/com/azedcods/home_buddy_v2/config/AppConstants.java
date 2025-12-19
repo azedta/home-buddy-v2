e4919be264e35970c561386fd4b406fb6c9157d1
Aziz Tazrout <aztazrout@gmail.com>
2025-12-19 16:03:55 -0500

    Add application configuration

---
+++ src/main/java/com/azedcods/home_buddy_v2/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.azedcods.home_buddy_v2.config;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_MEDICATIONS_BY = "medicationId";
+    public static final String SORT_DIR = "asc";
+}
